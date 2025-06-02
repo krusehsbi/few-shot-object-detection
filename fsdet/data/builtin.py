d8d25ef83ab983d596aec3ab27ec2d73321c3f1a
krusehsbi <alexander.kruse@hsbi.de>
2025-06-02 16:13:56 +0200

    added actual metadata

--- fsdet/data/builtin.py
+++ fsdet/data/builtin.py
@@ -295,13 +295,14 @@ def register_all_coco_taco(root="datasets"):
     )
 
     # NOVEL
+    metadata = _get_builtin_metadata("coco_taco")
 
     # 1shots
     register_coco_taco_dataset(
         name="taco_1shot_42_train",
         json_path="/content/fsdet/datasets/TACO/fewshot_splits/1shot/seed42/train.json",
         image_root="/content/fsdet/datasets/TACO",
-        thing_classes=[],
+        thing_classes=metadata["thing_classes"],
         metadata=_get_builtin_metadata("coco_taco_fewshot"),
     )
 
@@ -309,7 +310,7 @@ def register_all_coco_taco(root="datasets"):
         name="taco_1shot_42_val",
         json_path="/content/fsdet/datasets/TACO/fewshot_splits/1shot/seed42/val.json",
         image_root="/content/fsdet/datasets/TACO",
-        thing_classes=[],
+        thing_classes=metadata["thing_classes"],
         metadata=_get_builtin_metadata("coco_taco_fewshot"),
     )
 
